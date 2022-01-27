595fb2435f80c152d37758510f782eaf45427fd4
Felix Köpge <felix.koepge@schmidt-embedded-systems.de>
2022-01-27 21:53:02 +0100

    implement Sindy algorithm

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -25,7 +25,7 @@ object Sindy {
   //                    |   a   |     B     |
   //                    |  ...       ...    |
   //
-  private def readValueAttributePairs(input: String, spark: SparkSession): DataFrame = {
+  private def readValueAttributePairs(input: String, spark: SparkSession): Dataset[(String,String)] = {
     import spark.implicits._
 
     val table = readData(input, spark)
@@ -33,21 +33,35 @@ object Sindy {
     // uncomment to include file name in column name
     //val header = table.columns.map(col => s"$input[$col]")
 
+    // (a, b, c, d) => (a, A), (b, B), (c, C)
     table.flatMap(row => row.toSeq.asInstanceOf[Seq[String]].zip(header))
-      .withColumnRenamed("_1", "value")
-      .withColumnRenamed("_2", "attribute")
   }
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
     import spark.implicits._
 
-    // we chain the (value, attribute) tables of _all_ datasets
-    val pairs = inputs.map(i => readValueAttributePairs(i, spark))
-      .reduce((a, b) => a.union(b))
-    
-    pairs.show()
+    // see Data Profiling book p.74
 
+    // 1. Read
+    // 2. FlatMap
+    inputs.map(input => readValueAttributePairs(input, spark)) // read all datasets as (a, A)
+      .reduce((ds1, ds2) => ds1.union(ds2)) // append all rows together
+      .rdd // we now want to use RDD methods
+      .map(t => (t._1, Set(t._2))) // (a, A) => (a, {A})
 
-    // TODO
+      // 3. GroupBy+Aggregate
+      .reduceByKey((set1, set2) => set1 ++ set2) // where does the value occur: (a, {A}), (a, {B}) => (a, {A, B})
+      .map(t => t._2) // we don't need the value anymore: (a, {A, B}) => {A, B}
+      .filter(set => set.size > 1) // drop sets with only 1 element (value is only in 1 column = no INDs are possible)
+
+      // 4. FlatMap
+      .flatMap(set => set.map(elem => (elem, set - elem))) // all possible unary INDs: {(A, X\A) for A in X}
+
+      // 5. GroupBy+Aggregate
+      .reduceByKey((set1, set2) => set1.intersect(set2)) // (A, {B, C}), (A, {B, D}) => (A, {B})
+      .filter(t => t._2.size > 1) // drop entries for which no INDs were found
+      
+      // 6. Write
+      .collect.foreach(println) // print the resulting unary INDs to stdout
   }
 }
